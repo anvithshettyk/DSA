1ff6767f46be01759ad9dec903d25022b40f553f
anvith <223801002+anvithshettyk@users.noreply.github.com>
2026-07-01 18:57:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (94.36%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        int left = 0;
+        int right = s.size() - 1;
+
+        while(left < right) {
+
+            // skip non-alphanumeric from left
+            while(left < right && !isalnum(s[left])) {
+                left++;
+            }
+
+            // skip non-alphanumeric from right
+            while(left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            // compare lowercase characters
+            if(tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
