fe4c700f15892f8b2b63fa7659366204698ca5a6
anvith <223801002+anvithshettyk@users.noreply.github.com>
2026-07-03 22:02:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (63.40%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>ans;
+        for(int i=1;i<=numRows;i++)
+        {
+            ans.push_back(genraterow( i));
+        }
+        return ans;
+    }
+
+        vector<int>genraterow(int row){
+        long long ans=1;
+        vector<int>ansrow;
+        ansrow.push_back(ans);
+        for(int col=1;col<row;col++)
+        {
+            ans=ans*(row-col);
+            ans=ans/col;
+            ansrow.push_back(ans);
+        } 
+        return ansrow;
+    }
+
+        
+    };
