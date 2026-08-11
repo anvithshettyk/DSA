99feed927081c9a299f4135ac943117a6f40e673
anvith <223801002+anvithshettyk@users.noreply.github.com>
2026-08-11 18:44:27 +0530

    Time: 19 ms (41.05%) | Memory: 22 MB (30.76%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string res="";
+        int reslen=0;
+        for(int i=0;i<s.length();i++)
+        {
+            int l=i,r=i;
+            while(l>=0 &&r<s.length()&&s[l]==s[r])
+            {
+                if(r-l+1>reslen)
+                {
+                    res=s.substr(l,r-l+1);
+                    reslen=r-l+1;
+                }
+                l--;
+                r++;
+            }
+             l=i,r=i+1;
+            while(l>=0 &&r<s.length()&&s[l]==s[r])
+            {
+                if(r-l+1>reslen)
+                {
+                    res=s.substr(l,r-l+1);
+                    reslen=r-l+1;
+                }
+                l--;
+                r++;
+            }
+        }
+        return res;
+     
+    }
+};
