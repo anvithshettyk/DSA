3f0d10378636638b73671bffe2092f32b0dca2b3
anvith <223801002+anvithshettyk@users.noreply.github.com>
2026-09-06 16:54:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (21.36%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -0,0 +1,32 @@
+class MyStack {
+public:
+    queue<int> q;
+
+    MyStack() {
+    }
+    
+    void push(int x) {
+        int s = q.size();
+        
+        q.push(x);
+
+        for(int i = 0; i < s; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int ans = q.front();
+        q.pop();
+        return ans;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
