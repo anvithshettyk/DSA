b6dfb0246b44293f99834aa2e80fc93d49497fd1
anvith <223801002+anvithshettyk@users.noreply.github.com>
2026-09-21 12:24:27 +0530

    Time: 226 ms (8.85%) | Memory: 20.4 MB (99.49%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -23,13 +23,13 @@ class Solution:
         
             mid=(l+r)//2
             if(fun(mid)):
-                ans=mid
+               
             
                 r=mid-1
             
             else:
                 l=mid+1
 
-        return ans
+        return l
         
         
